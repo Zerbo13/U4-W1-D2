d88322969597cbbf06d062be2c1e34b9126bcf0e
Mattia Zerbini <zermatty6@gmail.com>
2026-01-07 15:24:50 +0100

    Add Es1 and Es3 with if else and while

--- U4-W1-D2/src/es1/Esercizio1.java
+++ U4-W1-D2/src/es1/Esercizio1.java
@@ -1,4 +1,42 @@
 package es1;
 
+import java.util.Scanner;
+
 public class Esercizio1 {
+
+    //PariDispari
+    public static boolean numeroStringa(String s) {
+        if (s.length() % 2 == 0) {
+            return true;
+        } else{
+                return false;
+        }
+    }
+
+    //Anno Bisestile
+    public static boolean annoBisestile(int anno) {
+        if (anno % 400 == 0) {
+            return true;
+        } else if (anno % 100 == 0) {
+            return false;
+        } else if (anno % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci una parola: ");
+        String testo = scanner.nextLine();
+        System.out.println("La lunghezza è pari? " + numeroStringa(testo));
+
+        System.out.print("Inserisci un anno: ");
+        int anno = scanner.nextInt();
+        System.out.println("L'anno bisestile? " + annoBisestile(anno));
+
+    }
 }
--- U4-W1-D2/src/es3/Esercizio3.java
+++ U4-W1-D2/src/es3/Esercizio3.java
@@ -1,4 +1,27 @@
 package es3;
 
+import java.util.Scanner;
+
 public class Esercizio3 {
-}
+    public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                String parola;
+
+                while (true) {
+                    System.out.print("Inserisci una parola, inserisci q per uscire: ");
+                    parola = scanner.nextLine();
+
+                    if (parola.equals("q")) {
+                        break;
+                    }
+
+                    for (int i = 0; i < parola.length(); i++) {
+                        System.out.print(parola.charAt(i));
+                        if (i < parola.length() - 1) {
+                            System.out.print(", ");
+                        }
+                    }
+                    System.out.println();
+                }
+            }
+        }
